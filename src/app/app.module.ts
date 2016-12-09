f42cc0cd3de813b619987b9e4605ab7b0e6cf69c
Brooke Storm <brooke@stormclan.com>
2016-12-08 22:28:07 -0700

    It almost works now with a menu bar and such

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent }  from './app.component';
+import { MenuBarComponent } from './menu/menubar.component';
 
 @NgModule({
   imports:      [ BrowserModule, NgbModule.forRoot() ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+                  MenuBarComponent
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/menu/menubar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'menubar',
+  templateUrl: './menubar.component.html',
+})
+export class MenuBarComponent {
+}
